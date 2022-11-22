6275cf4eb5d5655c7720ec5e41c50078eb3badb5
Luis Brayan <57547348+luisbrayan@users.noreply.github.com>
2022-11-22 17:03:10 -0500

    First Version Bootstrap

---
+++ Index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+	<title> Plantilla con Bootstrap</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- BOOTSTRAP-->
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Styles/styles.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+    
+	<script src="bootstrap/js/bootstrap.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.js"></script>    
+    
+
+</head>
+<body>
+	<header>
+		<?php 
+			require_once('Layouts/header.php');
+		?>	
+	</header>
+
+	<main>	
+		<div class="container">
+		<?php 				
+			//Containt main PHP Form
+		?>
+
+		</div>
+	</main>
+
+	<footer>
+		<?php 
+			include_once('Layouts/footer.php');
+		?>
+	</footer>
+</body>
+</html>
---
+++ Layouts/header.php
@@ -0,0 +1,46 @@
+<!-- As a heading -->
+<nav class="nav navbar navbar-nav navbar-light bg-light mx-auto">
+  <div class="container-fluid text-center">
+    <span class="navbar-brand mb-0 h1">Adopt Me</span>
+  </div>
+</nav>
+
+
+<nav class="navbar navbar-expand-lg " style="background-color: #ADB4BA;">
+  <div class="container-fluid">        
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Pets</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Search Pet</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">History Adoptions</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Acept Adoption</a>
+        </li>
+        
+      </ul>    
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item dropdown ml-auto">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Welcome User 1
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="#">Log Out</a></li>                
+            </ul>
+            </li>
+        </ul>  
+    </div>
+  </div>
+</nav>
+
